41bc03b7d5b8ca21676f94da38469b8f169e587c
prasadlvv <30382279+kprasad7@users.noreply.github.com>
2022-04-19 16:19:58 +0530

    updating name

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,7 +12,7 @@ def test_root(client):
     """Test the default route."""
 
     res = client.get('/')
-    assert b'Hello world!' in res.data
+    assert b'Hello world! Hi ferozekhan' in res.data
 
 
 def test_status(client):
